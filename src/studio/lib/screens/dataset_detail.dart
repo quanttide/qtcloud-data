f6b3a5e76535a67d17d9fc93c9ae50d59b0ed438
Guo Zhang <zhangguo@quanttide.com>
2026-01-15 15:53:10 +0800

    chore(studio): 更新前端依赖和测试
    - 更新 pubspec.lock 依赖版本
    - 更新 dataset_detail.dart 组件
    - 更新 widget_test.dart 测试文件

--- src/studio/lib/screens/dataset_detail.dart
+++ src/studio/lib/screens/dataset_detail.dart
@@ -1,14 +1,17 @@
 /// 数据集详情页面
 
-
 import 'package:flutter/material.dart';
 
 import '../models/datasets.dart';
 
-
-
 class DatasetDetailScreen extends StatefulWidget {
-  Dataset dataset = Dataset(name: 'universities', verboseName: '高校数据集', description: '高校及其院系列表', count: 1000);
+  Dataset dataset = Dataset(
+      name: 'universities',
+      verboseName: '高校数据集',
+      description: '高校及其院系列表',
+      count: 1000);
+
+  DatasetDetailScreen({super.key});
 
   // final Dataset dataset;
 
